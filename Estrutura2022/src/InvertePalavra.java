b98be5e544b357a49da047b25d93d467afcddcc0
Alunos CC Faesa 02 2021 <103604848+Faesacc2021@users.noreply.github.com>
2022-04-13 20:23:04 -0300

    Add files via upload

---
+++ Estrutura2022/src/InvertePalavra.java
@@ -0,0 +1,30 @@
+package pilha;
+
+import java.util.*;
+
+import dados.*;
+
+public class InvertePalavra {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Digite uma palavra:");
+		String palavra = teclado.next();
+		
+		PilhaContigChar pilha = new PilhaContigChar(palavra.length());
+		
+		for (int i = 0; i < palavra.length(); i++) {
+			pilha.empilhar(new ItemChar (palavra.charAt(i)));
+		}
+		
+		palavra = "";
+		
+		while (!pilha.eVazia()) {
+			palavra += pilha.desempilhar().getChave();
+		}
+		
+		System.out.println("Palava invertida:\n"+ palavra);
+	}
+
+}
